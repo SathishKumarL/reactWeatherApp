9ec95dd70f17a507f8ca24612c61893bfa621b9c
Sathish Kumar <l.sathish.kumar@accenture.com>
2021-08-04 18:35:13 +0530

    Updates

---
+++ precache-manifest.49c621c0bc4f2fdc209f89b1e418f951.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73a698dea20badd197647a87721d876",
+    "url": "/reactWeatherApp/index.html"
+  },
+  {
+    "revision": "8d226ec03cba64214b3c",
+    "url": "/reactWeatherApp/static/css/2.58a7a907.chunk.css"
+  },
+  {
+    "revision": "4265ba808e7471dd28d4",
+    "url": "/reactWeatherApp/static/css/main.d3cfc39e.chunk.css"
+  },
+  {
+    "revision": "8d226ec03cba64214b3c",
+    "url": "/reactWeatherApp/static/js/2.8b7a4032.chunk.js"
+  },
+  {
+    "revision": "5cf0b55f711b4765b7e42faa5df6636d",
+    "url": "/reactWeatherApp/static/js/2.8b7a4032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4265ba808e7471dd28d4",
+    "url": "/reactWeatherApp/static/js/main.030e7657.chunk.js"
+  },
+  {
+    "revision": "e518e143e64f7a0d3d7f",
+    "url": "/reactWeatherApp/static/js/runtime-main.e3b8c797.js"
+  },
+  {
+    "revision": "367aff04316368ec5334b54510a2e7d7",
+    "url": "/reactWeatherApp/static/media/weather-forecast-icon.367aff04.jpg"
+  }
+]);
